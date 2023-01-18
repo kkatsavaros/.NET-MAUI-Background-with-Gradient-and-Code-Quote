4abc0f44f8288c81ce8591bc82ca1f2093e84a80
kkats <kkats@KONSTANTINOS>
2023-01-18 12:48:00 +0200

    Add project files.

---
+++ CodeQuotes2/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace CodeQuotes2;
+
+public partial class MainPage : ContentPage
+{
+    List<string> quotes7 = new List<string>();  // Για να αλλάζει το Quote.
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    // Για να αλλάζει το Quote.
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await LoadMauiAsset();
+    }
+
+    async Task LoadMauiAsset()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+        using var reader = new StreamReader(stream);  // Read the File.
+
+        // var contents = reader.ReadToEnd(); // Για να βάλω το περιεχόμενο όλο σε μία μεταβλητή.
+
+        while (reader.Peek() != -1)
+        {
+            quotes7.Add(reader.ReadLine());
+        }
+
+    }
+
+    Random random = new Random();  // Για να αλλάζει το Background σε Gradient.
+
+    private void btngenerateQuote_Clicked(object sender, EventArgs e)
+    {
+        // Για να αλλάζει το Background σε Gradient.
+        var startColor = System.Drawing.Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+
+        var endColor = System.Drawing.Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+
+        var colors = ColorUtility.ColorControls.GetColorGradient(startColor, endColor, 6);  // 6 χρώματα
+
+
+        float stopOffset = .0f;
+
+        var stops = new GradientStopCollection();
+
+        foreach (var c in colors)
+        {
+            stops.Add(new GradientStop(Color.FromArgb(c.Name), stopOffset));
+            stopOffset += .2f;
+        }
+
+        var gradient = new LinearGradientBrush(stops, new Point(0, 0), new Point(1, 1));
+
+        background.Background = gradient;
+
+
+        // Για να αλλάζει το Quote.
+        int index = random.Next(quotes7.Count);
+        quotesLabel.Text = quotes7[index];                //  x:Name="quotesLabel"
+
+    }
+
+}
+
---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+.NET MAUI Background with Gradient and Code Quote
+
+
+With that app we press a button and we can change dynamically the background color and the Quote of the app.
+
+The background color has a linear gradient.
+
+There is also a color random generator.
+
+I use also the Color build in class. 
+
+
+
+Katsavaros Konstantinos
+
